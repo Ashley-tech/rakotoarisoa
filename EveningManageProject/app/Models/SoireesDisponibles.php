38fc3c33c419b92d85284aa11533ac6521538448
Ashley R. <ashleyrakotoarisoa@gmail.com>
2024-11-08 16:24:26 +0100

    Début de la mise en place d'une vue matérialisée

---
+++ EveningManageProject/app/Models/SoireesDisponibles.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SoireesDisponibles extends Model
+{
+    protected $table = 'vue_soirees_disponibles';
+    public $incrementing = false;
+    public $timestamps = false;
+}
---
+++ EveningManageProject/database/migrations/2024_11_08_150713_soirees_view.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement("
+            CREATE MATERIALIZED VIEW evenings_view AS
+            SELECT s.id_soiree, s.titre_soiree, s.date_soiree, s.heure, a.ville AS ville_soiree, s.nbre_places_max,
+                COUNT(ea.id_participation) AS participants_actuels
+            FROM Soiree s
+            JOIN Adresse a ON s.id_adresse = a.id_adresse
+            JOIN Compte c ON c.id_adresse = a.id_adresse
+            LEFT JOIN soiree_participation ea ON s.id_soiree = ea.id_soiree
+            GROUP BY s.id_soiree, s.titre_soiree, s.date_soiree, s.heure, a.ville, s.nbre_places_max
+            HAVING COUNT(ea.id_participation) < s.nbre_places_max
+            ORDER BY s.date_publication;
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement("DROP MATERIALIZED VIEW IF EXISTS vue_soirees_disponibles");
+    }
+
+    public function refreshSoireesDisponibles(): void
+    {
+        DB::statement("REFRESH MATERIALIZED VIEW vue_soirees_disponibles");
+    }
+};
