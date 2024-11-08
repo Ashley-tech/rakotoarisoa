89fd20f25c692957f753e9a926113a4e8f032612
Ashley R. <ashleyrakotoarisoa@gmail.com>
2024-11-08 14:38:51 +0100

    Ajout routes pour inscription

--- EveningManageProject/routes/web.php
+++ EveningManageProject/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/connexion', function () {
+    
+});
+
+Route::get('/soirees', function() {
+
+});
+
+Route::get('/inscription', function() {
+    return view('inscription');
+});
